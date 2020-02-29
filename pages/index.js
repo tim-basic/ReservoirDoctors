eb7e18de8eae1ef3fdead742ec341182dd6d9e48
Tim Barbarino <tim@digitalhut.com.au>
2020-02-29 17:04:43 +1100

    resolving merge conflict

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,14 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Media from "react-media";
+<<<<<<< Updated upstream
+=======
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+>>>>>>> Stashed changes
 //Utils
 import axios from "../node_modules/axios";
 import { width } from "dom-helpers";
@@ -21,6 +29,10 @@ export default class extends Component {
       <React.Fragment>
         <Hero title={props.title} subtitle={props.subtitle} />
         <Staff staff={props.staff} />
+<<<<<<< Updated upstream
+=======
+        <VerticalTabs />
+>>>>>>> Stashed changes
       </React.Fragment>
     );
   }
@@ -45,7 +57,12 @@ function Hero(props) {
           style={headingStyles}
           variant="h1"
           component="h2"
+<<<<<<< Updated upstream
           gutterBottom>
+=======
+          gutterBottom
+        >
+>>>>>>> Stashed changes
           {props.subtitle}
         </Typography>
       </Container>
@@ -144,3 +161,102 @@ function StaffTitle(props) {
     </React.Fragment>
   );
 }
+<<<<<<< Updated upstream
+=======
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    flexDirection: "row"
+    // height: 224
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`
+  }
+}));
+
+function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Container>
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          // variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Item Four" {...a11yProps(3)} />
+          <Tab label="Item Five" {...a11yProps(4)} />
+          <Tab label="Item Six" {...a11yProps(5)} />
+          <Tab label="Item Seven" {...a11yProps(6)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          Item Seven
+        </TabPanel>
+      </div>
+    </Container>
+  );
+}
+>>>>>>> Stashed changes
